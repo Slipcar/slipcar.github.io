5a89b5d363dde1717cca551a42ece7575bf06d7f
Luis Carlos Souza <you@example.comcarlito.luiscarlos@gmail.com>
2021-07-30 01:33:30 -0300

    deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tasklist'));
+app.use(express.static('./dist/front-end'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/tasklist/' }),
+  res.sendFile('index.html', { root: 'dist/front-end/' }),
 );
 
 // Start the app by listening on the default Heroku port
